3d90ca9ba02587144805e8d9cae73285084be4cf
Ng Yat Yan <ng.yatyan@gmail.com>
2021-10-17 17:27:38 +0800

    Run hello message regularly

--- src/main/java/com/example/Greeter.java
+++ src/main/java/com/example/Greeter.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class Greeter {
 
-  public void greet(String name) {
-    System.out.println("Hello, " + name);
+  public void greet(String name, Object caller) {
+    System.out.println("Hello, " + name + ". I am " + caller + ". Nice to Meet You.");
   }
 }
--- src/main/scala/com/example/HelloScala.scala
+++ src/main/scala/com/example/HelloScala.scala
@@ -1,8 +1,21 @@
 package com.example
 
+case class Plugin(firstname: String, surname: String)
+
 object HelloScala extends App {
-  if (args.size == 0)
-    new Greeter().greet("Scala-Maven-Shade")
-  else
-    new Greeter().greet(args(0))
+
+  // notice the equal sign after the 2nd Unit
+  protected def runEveryMillis(callback: () => Unit, delay: Int): Unit = {
+    while (true) { callback(); Thread sleep delay }
+  }
+
+  override def toString() = Plugin("Scala", "Greeter").copy(surname = "Maven").toString()
+
+  runEveryMillis(() => {
+    if (args.size == 0) {
+      new Greeter().greet("Developer", this)
+    } else {
+      new Greeter().greet(args(0), this)
+    }
+  }, 1000)
 }
